29182c544b31b53264fc501906b3852803aedaaf
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-11-05 17:38:55 -0500

    Implement About section

--- src/modules/Ui.js
+++ src/modules/Ui.js
@@ -1,16 +1,27 @@
 class UI {
   static activateUi() {
     this.activateDarkmodeToggle();
+    this.activateNavBar();
   }
 
   static activateDarkmodeToggle() {
     const checkbox = document.querySelector("#dark-mode");
     const body = document.querySelector("body");
     const content = document.querySelector(".content");
+    const nav = document.querySelector("nav");
 
     checkbox.addEventListener("click", () => {
       body.classList.toggle("dark");
       content.classList.toggle("dark");
+      nav.classList.toggle("dark");
+    });
+  }
+
+  static activateNavBar() {
+    const about = document.querySelector("#about");
+    const aboutSection = document.querySelector(".about-section");
+    about.addEventListener("click", () => {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
     });
   }
 }
