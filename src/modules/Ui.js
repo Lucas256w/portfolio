568347ae8c0d802cba62e012ed67d1b91ce62d7d
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-11-05 20:02:52 -0500

    Implement contact section and fix svgs.

--- src/modules/Ui.js
+++ src/modules/Ui.js
@@ -10,19 +10,57 @@ class UI {
     const body = document.querySelector("body");
     const content = document.querySelector(".content");
     const nav = document.querySelector("nav");
+    const github = document.querySelectorAll("#github-icon");
+    const linkedin = document.querySelector("#linkedin-icon");
+    const svgIcons = document.querySelectorAll(".svg-icons");
+
+    if (checkbox.checked) {
+      body.classList.toggle("dark");
+      content.classList.toggle("dark");
+      nav.classList.toggle("dark");
+      github.forEach((git) => {
+        git.classList.toggle("dark");
+      });
+      svgIcons.forEach((svg) => {
+        svg.classList.toggle("dark");
+      });
+      linkedin.classList.toggle("dark");
+    }
 
     checkbox.addEventListener("click", () => {
       body.classList.toggle("dark");
       content.classList.toggle("dark");
       nav.classList.toggle("dark");
+      github.forEach((git) => {
+        git.classList.toggle("dark");
+      });
+      svgIcons.forEach((svg) => {
+        svg.classList.toggle("dark");
+      });
+      linkedin.classList.toggle("dark");
     });
   }
 
   static activateNavBar() {
     const about = document.querySelector("#about");
     const aboutSection = document.querySelector(".about-section");
+
+    const projects = document.querySelector("#projects");
+    const projectSection = document.querySelector(".projects-section");
+
+    const contact = document.querySelector("#contact");
+    const contactSection = document.querySelector(".contact-section");
+
     about.addEventListener("click", () => {
       aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+
+    projects.addEventListener("click", () => {
+      projectSection.scrollIntoView({ behavior: "smooth" });
+    });
+
+    contact.addEventListener("click", () => {
+      contactSection.scrollIntoView({ behavior: "smooth" });
     });
   }
 
